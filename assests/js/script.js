ad73c0ff4378fb8ca764251efa54ee1e9c6d541c
Dyego27 <dyegoid123@gmail.com>
2025-06-03 18:16:34 -0300

    Slideshow adicionado

--- assests/js/script.js
+++ assests/js/script.js
@@ -8,3 +8,46 @@ const menuShow = () => {
   openMenuIcon.classList.toggle("hide");
   closeMenuIcon.classList.toggle("show");
 };
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Avança/retrocede os slides
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Define o slide atual
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide-img");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    slides[i].classList.remove("active");
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("active");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  slides[slideIndex - 1].classList.add("active");
+
+  if (dots.length > 0) {
+    dots[slideIndex - 1].classList.add("active");
+  }
+}
